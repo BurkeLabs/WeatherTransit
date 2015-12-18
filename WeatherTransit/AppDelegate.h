3202b3c03b0fd7d482b826ec21a14debee2b3b3e
Michelle Burke <michele.burke01@gmail.com>
2015-12-17 19:27:22 -0600

    Initial Commit

---
+++ WeatherTransit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherTransit
+//
+//  Created by Michelle Burke on 12/17/15.
+//  Copyright © 2015 Burke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherTransit/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  WeatherTransit
+//
+//  Created by Michelle Burke on 12/17/15.
+//  Copyright © 2015 Burke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
